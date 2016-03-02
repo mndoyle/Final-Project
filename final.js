51a24167296fb93ca874c7bd58c4b0b8b6c6532a
Mary Doyle <mndoyle@ucdavis.edu>
2016-03-01 16:54:18 -0800

    update

--- final.js
+++ final.js
@@ -1,24 +1,68 @@
-$(function(){
+// Main Page
+
+// Slide Button Nav
+$(function () {
     console.log('ready');
-    
-    $('#helper').click(function(){
+
+    $('#swipeButton').click(function () {
         console.log('click');
-        
-        $('section#overlay').animate({
-            width:'100%',
-            left:'0',
+        $('#swipeButton').animate({
+            left: 104,
+        })
+        $('section#navOverlay').animate({
+            width: '80%',
+            left: '0',
         });
-        
-        $('section p').css('visibility','visible');
-        return false;  
+        $('section#navOverlay p').css('visibility', 'visible').animate({
+                opacity: 1,
+            },
+            1500
+        );
+
+        $('#swipeClose').css('visibility', 'visible');
+        return false;
+    });
+
+    $('section#navClose').click(function () {
+        console.log('click');
+        $('section#navOverlay').animate({
+            width: '0%',
+            left: '0',
+        });
+
+        $('#swipeButton').animate({
+            left: -210,
+        })
+        $('section#navOverlay p').css('visibility', 'hidden');
+        $('#swipeClose').css('visibility', 'hidden');
+        $('#swipeButton').css('visibility', 'visible');
+        return false;
     });
     
-    $('nav#close').click(function(){
+// Hamburger Helper Nav
+    $('#helper').click(function () {
         console.log('click');
-        $('section#overlay').css('width','0px');
-         $('section p').css('visibility','hidden');
-        return false;  
-    });   
+        
+        var center=$(window).width()/2;
+         $('section#overlay').css('left', center);
+
+        $('section#overlay').animate({
+            width: '100%',
+            left: '0',
+        });
+
+        $('section p').css('visibility', 'visible');
+        return false;
+    });
+
+    $('nav#close').click(function () {
+        console.log('click');
+        $('section#overlay').css('width', '0px');
+        $('section p').css('visibility', 'hidden');
+        return false;
+    });
 })
-    
-                
+
+// Restaurants
+
+// Sort By
